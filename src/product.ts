4a471c80925dddeaade22e1ec20ff6bae68ec46d
shaodado <shaoshineshine@gamil.com>
2024-04-11 07:26:35 +0800

    feat(ts): 變更產品建構式

---
+++ src/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  constructor(
+    public id: number,
+    public name: string,
+    public company: string,
+    public price: number,
+    public isShow: boolean,
+    public createDate: Date,
+    public modifyDate: undefined | Date
+  ) {}
+}
