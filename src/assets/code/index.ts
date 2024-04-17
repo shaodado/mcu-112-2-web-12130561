ac0a3e34060e22811d32840a73fc4327f7e4392d
shaodado <shaoshineshine@gamil.com>
2024-04-17 16:54:32 +0800

    chore(project): 移動程式檔至 assets 資料夾內

---
+++ src/assets/code/index.ts
@@ -0,0 +1,29 @@
+import { Product } from "./product";
+
+const products = [
+  new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024 - 3 - 10)
+  ),
+  new Product(
+    2,
+    "金魚都能懂的 CSS 必學屬性",
+    "博碩文化",
+    500,
+    true,
+    new Date(2024 - 3 - 10)
+  ),
+];
+
+setProductDisplay(products[1], false);
+
+console.log(products);
+console.table(products);
+
+function setProductDisplay(product: Product, isShow: boolean): void {
+  product.isShow = isShow;
+}
