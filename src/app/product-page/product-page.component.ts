b8cee83aaaaf17b8d78681bf09fbc25abdba4c26
shaodado <shaoshineshine@gamil.com>
2024-05-14 23:06:24 +0800

    feat(service): 從服務取得產品資料

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../model/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
+import { ProductService } from './../services/product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -10,51 +11,16 @@ import { ProductCardListComponent } from '../product-card-list/product-card-list
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
-export class ProductPageComponent {
-  products = [
-    new Product({
-      id: 1,
-      Name: '書籍 A',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 2,
-      Name: '書籍 B',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 3,
-      Name: '書籍 C',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-    new Product({
-      id: 4,
-      Name: '書籍 D',
-      authors: ['作者甲', '作者乙', '作者丙'],
-      company: '博碩文件',
-      isShow: true,
-      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text:img',
-      createDate: new Date(),
-      price: 10000,
-    }),
-  ];
+export class ProductPageComponent implements OnInit {
   router = inject(Router);
 
+  private ProductService!: ProductService;
+
+  products!: Product[];
+  ngOnInit(): void {
+    this.ProductService = new ProductService();
+    this.products = this.ProductService.getList();
+  }
   onEdit(product: Product): void {
     this.router.navigate(['product', 'form', product.id]);
   }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../model/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private_data = [
+    new Product({
+      id: 1,
+      Name: '書籍 A',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文件',
+      isShow: true,
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: 10000,
+    }),
+    new Product({
+      id: 2,
+      Name: '書籍 B',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文件',
+      isShow: true,
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: 10000,
+    }),
+    new Product({
+      id: 3,
+      Name: '書籍 C',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文件',
+      isShow: true,
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: 10000,
+    }),
+    new Product({
+      id: 4,
+      Name: '書籍 D',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文件',
+      isShow: true,
+      imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date(),
+      price: 10000,
+    }),
+  ];
+
+  getList(): Product[] {
+    return this.private_data;
+  }
+}
